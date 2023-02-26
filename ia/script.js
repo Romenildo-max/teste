857064debeb7d98787e9a7594dc8aba4e892c537
Romenildo-max <romenildo2017@outlook.com>
2023-02-26 19:35:41 -0300

    atualização

--- ia/script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector("#hamburger");
-const nav = document.querySelector("nav");
-
-hamburger.addEventListener("click", function() {
-  nav.style.display = nav.style.display === "block" ? "none" : "block";
-});
